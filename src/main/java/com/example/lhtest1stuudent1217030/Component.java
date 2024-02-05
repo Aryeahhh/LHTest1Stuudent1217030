07e0d81f778625c9783323e9d210ecd2183e6b71
Aryeahhh <aryapatel0107@gmail.com>
2024-02-05 13:30:06 -0500

    Question 2.1 Complete!

---
+++ src/main/java/com/example/lhtest1stuudent1217030/Component.java
@@ -0,0 +1,13 @@
+package com.example.lhtest1stuudent1217030;
+
+public class Component {
+    private String componentName;
+    private String manufacturer;
+    private double price;
+    public Component(String componentName, String manufacturer, double price) {
+        this.componentName = componentName;
+        this.manufacturer = manufacturer;
+        this.price = price;
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.lhtest1stuudent1217030 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.lhtest1stuudent1217030 to javafx.fxml;
+    exports com.example.lhtest1stuudent1217030;
+}
